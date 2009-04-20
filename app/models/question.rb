a49f079606598ff3507da7d585e724cc7f67e9ac
James Somers <jsomers@gmail.com>
2009-04-20 16:55:54 -0400

    Added category function

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,12 @@
 class Question < ActiveRecord::Base
+  def category
+    g = Game.find_by_game_id(self.game_id)
+    single = CGI.unescapeHTML(g.categories).split('^')[1..6]
+    double = CGI.unescapeHTML(g.categories).split('^')[7..-2]
+    if self.coord.include? 'DJ'
+      return double[self.coord.split(',')[1].to_i - 1]
+    else
+      return single[self.coord.split(',')[1].to_i - 1]
+    end
+  end
 end
