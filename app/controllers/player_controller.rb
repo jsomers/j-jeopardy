419e36c2a3175f05a72c0c00acaf7ebd1942529a
James Somers <jsomers@gmail.com>
2009-08-04 01:14:47 -0400

    Add the remove link to regularly rendered validation output

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -31,7 +31,7 @@ class PlayerController < ApplicationController
       render :text => "<div style='margin-left: -30px; padding: 10px; border: 1px solid #eee; background: #ffffee; font-weight: bold;'>" + handle + " <span style='color: #999; font-size: 20px; font-weight: normal;'>(use the <strong>#{keys[plyr]}</strong> key to buzz in)</span><a href='#' onclick='remove_player(#{plyr - 1})' style='float: right; vertical-align: middle; text-decoration: none;'>[X]</a></div><br/>"
     elsif p and p.password_matches?(password) and !session[:players].include? p.id.to_s
       session[:players][plyr - 1] = p.id.to_s
-      render :text => "<div style='margin-left: -30px; padding: 10px; border: 1px solid #eee; background: #ffffee; font-weight: bold;'>" + handle + " <span style='color: #999; font-size: 20px; font-weight: normal;'>(use the <strong>#{keys[plyr]}</strong> key to buzz in)</span></div><br/>"
+      render :text => "<div style='margin-left: -30px; padding: 10px; border: 1px solid #eee; background: #ffffee; font-weight: bold;'>" + handle + " <span style='color: #999; font-size: 20px; font-weight: normal;'>(use the <strong>#{keys[plyr]}</strong> key to buzz in)</span><a href='#' onclick='remove_player(#{plyr - 1})' style='float: right; vertical-align: middle; text-decoration: none;'>[X]</a></div><br/>"
     else
       render :partial => "play/start_spot", :locals => {:player => plyr.to_s, :failed => true}
     end
