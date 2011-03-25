e4e3d4f8665e2ec1a25c5e3139dea2895b418d6f
James Somers <jsomers@gmail.com>
2011-03-24 22:19:27 -0400

    Add season 27 to Blast

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,19 @@
 default_run_options[:pty] = true
 set :application, "jeopardy"
+
 set :repository,  "git@github.com:jsomers/j-jeopardy.git"
 set :scm, "git"
-# Command to load new code: touch /home/james/public_html/jeopardy/current/tmp/restart.txt
-# set :scm_passphrase, "D0nkeyB0nkers!" # This is your custom user's password
 set :branch, "master"
 set :user, "james"
+
 set :deploy_via, :remote_cache
 set :port, 30000
-
 set :runner, "james"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/home/james/public_html/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
 
 set :location, "jimbotronimo.com"
 role :app, location
